1257053740cb3f2a71bfd9f0f94f01a701a5d5a4
Jason <jasonpaff@gmail.com>
2023-04-01 19:09:58 -0400

    Storybook, eslint, prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-        Hello World
+    <div
+      className={
+        "flex min-h-screen w-full items-center justify-center bg-slate-800 text-gray-100"
+      }
+    >
+      Hello World
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
